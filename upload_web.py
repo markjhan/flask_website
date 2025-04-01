ec0d7391159207ebe0303e5143422cfa8a8840e0
Mark J Han <markjuhan09@gmail.com>
2025-04-01 11:38:42 -0700

    fix port of Render

--- upload_web.py
+++ upload_web.py
@@ -22,4 +22,6 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=9090)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port = port)
