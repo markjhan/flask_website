2f2e2b2f2e0b9db9d1672733b202350e0e3b18aa
Mark J Han <markjuhan09@gmail.com>
2025-04-03 11:45:03 -0700

    Update upload_web.py

--- upload_web.py
+++ upload_web.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
@@ -9,9 +8,8 @@ def home():
     <h1>this is main page<h1>
     <a href="https://www.google.com" target='_blank'>go to google</a> >:)<br>
     <a href="/about"> go to about page </a>
-
+    <a href="/message">我要留言</a><br>
     '''
-
 
 @app.route('/about')
 def about():
@@ -20,8 +18,34 @@ def about():
     <a href="/">back to main page</a>
     '''
 
+@app.route('/message')
+def message():
+    return '''
+    <h1>留言板</h1>
+    <form action="/submit" method="POST">
+        你的名字：<br>
+        <input type="text" name="name"><br><br>
+
+        留言内容：<br>
+        <textarea name="message" rows="4" cols="40"></textarea><br><br>
+
+        <input type="submit" value="提交留言">
+    </form>
+    <br>
+    <a href="/">返回主页</a>
+    '''
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    msg = request.form.get('message')
+
+    with open("messages.txt", "a", encoding='utf-8') as f:
+        f.write(f"{name}：{msg}\n")
+
+    return f"谢谢你的留言，{name}！<br><a href='/'>返回主页</a>"
+
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port = port)
+    app.run(debug=True, host='localhost', port=9090)
+
