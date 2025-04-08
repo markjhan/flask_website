94b7ac08a8606f002e7296a19fa6e3e16dbde9bf
Mark J Han <markjuhan09@gmail.com>
2025-04-07 22:56:32 -0700

    fix

--- upload_web.py
+++ upload_web.py
@@ -11,10 +11,39 @@ def home():
     <a href="/message">Leave some message</a><br>
     '''
 
+@app.route('/secret')
+def secret():
+    return '''
+    <h1>You find the secret page!<h1>
+    <form action="/dunbar" method="POST">
+        Your Name: <br>
+        <input type="text" name="name"><br><br>
+        
+        SecretMessage: <br>
+        <textarea name="secret message" rows="4" cols="40"></textarea><br><br>
+        
+        <input type="submit" value="Submit">
+    </form>
+    <br>
+    <a href="/">Back to Main Page</a>
+    '''
+
+@app.route('/dunbar', methods=['POST'])
+def dunbar():
+    name = request.form.get('name')
+    msg = request.form.get('secret message')
+
+    print(f"Receive：{name} - {msg}")
+
+    with open("secret_messages.txt", "a", encoding='utf-8') as f:
+        f.write(f"{name} - {msg}\n")
+
+    return f"Thank you for your secret, {name}! <br><a href='/'>Back to Main Page</a>"
+
 @app.route('/about')
 def about():
     return '''
-    <h1>This is About web，page！<h1>
+    <h1>This is About page！<h1>
     <a href="/">back to main page</a>
     '''
 
@@ -32,9 +61,15 @@ def message():
         <input type="submit" value="Submit">
     </form>
     <br>
-    <a href="/">Back to Main Page</a>
+    <a href="/">Back to Main Page</a><br>
+    <a href="/sure">Something else here(?)</a>
     '''
 
+@app.route('/sure')
+def sure():
+    return '''
+    <h1>ARE YOU SURE YOU WANT TO SEE  THIS PAGE??<h1><br>
+    <a href="/secret">I AM SURE</a>'''
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
